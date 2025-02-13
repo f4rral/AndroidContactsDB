b589e6a9fe0cec2d6b2aa1fa1b8d8e3d51fef146
f4rral <f4rral@gmail.com>
2025-02-14 01:30:32 +0300

    Вывод

--- app/src/main/java/com/example/contactsdb/ui/screens/home/Home.kt
+++ app/src/main/java/com/example/contactsdb/ui/screens/home/Home.kt
@@ -1,19 +1,33 @@
 package com.example.contactsdb.ui.screens.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.contactsdb.viewmodels.ContactsViewModel
 
 @Composable
 fun HomeScreen() {
-    Box(
+    val vmContact: ContactsViewModel = viewModel(factory = ContactsViewModel.factory)
+    val contactList = vmContact.contactList.collectAsState(initial = emptyList()).value
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
         Text(text = "HomeScreen")
+
+        LazyColumn {
+            items(contactList) { contact ->
+                Text(text = contact.name)
+            }
+        }
     }
 }
 
