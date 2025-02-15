ca3e2da7f662fd80512a4890c32f2f48385f5435
f4rral <f4rral@gmail.com>
2025-02-15 18:20:30 +0300

    Домашняя страница

--- app/src/main/java/com/example/contactsdb/ui/screens/home/Home.kt
+++ app/src/main/java/com/example/contactsdb/ui/screens/home/Home.kt
@@ -2,14 +2,16 @@ package com.example.contactsdb.ui.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.contactsdb.data.Contact
+import com.example.contactsdb.ui.contact.ContactList
+import com.example.contactsdb.ui.contact.ContactUI
 import com.example.contactsdb.viewmodels.ContactsViewModel
 
 @Composable
@@ -17,22 +19,36 @@ fun HomeScreen() {
     val vmContact: ContactsViewModel = viewModel(factory = ContactsViewModel.factory)
     val contactList = vmContact.contactList.collectAsState(initial = emptyList()).value
 
+    HomeBody(
+        contactList
+    )
+}
+
+@Composable
+fun HomeBody(
+    contactList: List<Contact>,
+    title: String = "HomeBody"
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Text(text = "HomeScreen")
+        Text(
+            text = title,
+            fontSize = 26.sp
+        )
 
-        LazyColumn {
-            items(contactList) { contact ->
-                Text(text = contact.name)
-            }
-        }
+        ContactList(
+            contactList = contactList
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    HomeBody(
+        contactList = ContactUI.previewContactData,
+        title = "HomeScreenPreview"
+    )
 }
